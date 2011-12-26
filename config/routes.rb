a58739a3da3540720c5c2d02a358b37ef7690cea
Varun Narang <varunarang@gmail.com>
2011-12-26 18:18:18 +0530

    Sign-In-Out partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 FirstApp::Application.routes.draw do
 
+  get "sessions/new"
+
   get "users/new"
 
-	match '/signup', :to => 'users#new'
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
  
   match "/contact", :to => "pages#contact"
 
